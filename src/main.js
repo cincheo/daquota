34b7754a0e9cd1a882415dfea4d6dc3b8af362fb
Renaud Pawlak <renaud.pawlak@gmail.com>
2021-07-02 12:34:33 +0200

    add google sign in

--- src/main.js
+++ src/main.js
@@ -794,16 +794,16 @@ function start() {
             }
         },
         methods: {
-            onSignIn(googleUser) {
-                let profile = googleUser.getBasicProfile();
-                console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-                console.log('Name: ' + profile.getName());
-                console.log('Image URL: ' + profile.getImageUrl());
-                console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-                this.loggedIn = true
+            signIn() {
+                // let profile = googleUser.getBasicProfile();
+                // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+                // console.log('Name: ' + profile.getName());
+                // console.log('Image URL: ' + profile.getImageUrl());
+                // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+                // this.loggedIn = true
             },
             signOut() {
-                var auth2 = gapi.auth2.getAuthInstance();
+                let auth2 = gapi.auth2.getAuthInstance();
                 auth2.signOut().then(() => {
                     console.log('User signed out.');
                     this.loggedIn = false;
