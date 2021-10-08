4c6b794439259b4afd4a098dcd973d6483f1d74b
Renaud Pawlak <renaud.pawlak@gmail.com>
2021-10-08 09:05:39 +0200

    more explicit message

--- src/data-providers/application-connector.js
+++ src/data-providers/application-connector.js
@@ -4,7 +4,7 @@ Vue.component('application-connector', {
         <div :id="cid" :style="componentBorderStyle()">
             <component-icon v-if="isEditable()" :type="viewModel.type"></component-icon>
             <component-badge :component="getThis()" :edit="isEditable()" :targeted="targeted" :selected="selected"></component-badge>
-            <b-alert variant="warning" show>Unavailable component</b-alert>
+            <b-alert v-if="edit" variant="warning" show>Unavailable - please activate the <b>backend4dLite plugin</b> in order to load this connector</b-alert>
         </div>
     `,
     methods: {
