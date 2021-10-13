e506c7942b07dc323487f792af4a133ec510046a
Renaud Pawlak <renaud.pawlak@gmail.com>
2021-10-13 07:17:45 +0200

    improve sync/share protocol

--- dist/sync_download.php
+++ dist/sync_download.php
@@ -1,5 +1,8 @@
 <?php
 
+    include 'config.php';
+    include 'rest_headers.php';
+
     function str_ends_with( $haystack, $needle ) {
         $length = strlen( $needle );
         if( !$length ) {
@@ -8,31 +11,7 @@
         return substr( $haystack, -$length ) === $needle;
     }
 
-    // Allow from any origin
-    if (isset($_SERVER['HTTP_ORIGIN'])) {
-        // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-        // you want to allow, and if so:
-        header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-        header('Access-Control-Allow-Credentials: true');
-        #header('Access-Control-Max-Age: 86400');    // cache for 1 day
-    }
-    
-    // Access-Control headers are received during OPTIONS requests
-    if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
-            // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");         
-        
-        if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
-            header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
-    
-        exit(0);
-    }
-    
-    header('Content-type: application/json');
-
-    $dir = '../dlite/'.$_GET['user'];
+    $dir = $SYNC_DATA_DIR.'/'.$_GET['user'];
     if (!is_dir($dir)) {
       mkdir($dir);
     }
