b463c2dbb2ec2fc15013faf02537c6c93ea7ece9
Renaud Pawlak <renaud.pawlak@gmail.com>
2021-11-11 08:01:58 +0100

    fix SplitView

--- src/layouts/split-view.js
+++ src/layouts/split-view.js
@@ -9,27 +9,27 @@ Vue.component('split-view', {
             <component-badge :component="getThis()" :edit="isEditable()" :targeted="targeted" :selected="selected"></component-badge>
             <b-row id="mainRow" v-if="viewModel.orientation === 'VERTICAL'">
                 <b-col :style="componentBorderStyle()">
-                    <component-view :cid="viewModel.primaryComponent.cid" keyInParent="primaryComponent" :inSelection="isEditable()" />
+                    <component-view :cid="viewModel.primaryComponent?.cid" keyInParent="primaryComponent" :inSelection="isEditable()" />
                 </b-col>
                 <b-col :style="componentBorderStyle()">
-                    <component-view :cid="viewModel.secondaryComponent.cid" keyInParent="secondaryComponent" :inSelection="isEditable()" />
+                    <component-view :cid="viewModel.secondaryComponent?.cid" keyInParent="secondaryComponent" :inSelection="isEditable()" />
                 </b-col>
             </b-row>
             <b-row id="primaryRow" v-if="viewModel.orientation === 'HORIZONTAL'">
                 <b-col :style="componentBorderStyle()">
-                    <component-view :cid="viewModel.primaryComponent.cid" keyInParent="primaryComponent" :inSelection="isEditable()" />
+                    <component-view :cid="viewModel.primaryComponent?.cid" keyInParent="primaryComponent" :inSelection="isEditable()" />
                 </b-col>
             </b-row>                
             <b-row id="secondaryRow" v-if="viewModel.orientation === 'HORIZONTAL'">
                 <b-col :style="componentBorderStyle()">
-                    <component-view :cid="viewModel.secondaryComponent.cid" keyInParent="secondaryComponent" :inSelection="isEditable()" />
+                    <component-view :cid="viewModel.secondaryComponent?.cid" keyInParent="secondaryComponent" :inSelection="isEditable()" />
                 </b-col>
             </b-row>
         </b-container>
     `,
     methods: {
         propNames() {
-            return ["cid", "class", "style", "dataSource", "orientation", "primaryComponent", "secondaryComponent"];
+            return ["cid", "class", "style", "dataSource", "field", "orientation", "primaryComponent", "secondaryComponent"];
         },
         customPropDescriptors() {
             return {
