54b63bc74a4596766bf707831de8b65b5ff16325
Renaud Pawlak <renaud.pawlak@gmail.com>
2021-12-04 22:11:07 +0100

    add empty variant

--- src/components/button-view.js
+++ src/components/button-view.js
@@ -58,6 +58,7 @@ Vue.component('button-view', {
                     type: 'select',
                     editable: true,
                     options: [
+                        "",
                         "primary", "secondary", "success", "danger", "warning", "info", "light", "dark",
                         "outline-primary", "outline-secondary", "outline-success", "outline-danger", "outline-warning", "outline-info", "outline-light", "outline-dark",
                         "link"
