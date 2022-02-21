dcc5b8c097d6f8669ceffefac92fb37e276d4609
Renaud Pawlak <renaud.pawlak@gmail.com>
2022-02-21 07:53:37 +0100

    add zapier api

---
+++ src/api/zapier/authenticate.php
@@ -0,0 +1,35 @@
+<?php
+
+    include '../config.php';
+    include '../rest_headers.php';
+
+    $authorized = $_POST['user'] == 'admin' && $_POST['password'] == $ADMIN_PASSWORD;
+    if (!$authorized) {
+        $file = $SYNC_DATA_DIR.'/admin/users.json';
+        $users = json_decode(json_decode(file_get_contents($file), true)['data'], true);
+        $index = array_search($_POST['user'], array_column($users, 'login'));
+        if ($index !== false && $users[$index]['password'] == $_POST['password']) {
+            $authorized = true;
+            $user = $users[$index];
+            unset($user['password']);
+        }
+    } else {
+        $user = [];
+        $user['id'] = 'admin';
+        $user['login'] = 'admin';
+        $user['firstName'] = $ADMIN_FIRSTNAME;
+        $user['lastName'] = $ADMIN_LASTNAME;
+        $user['email'] = $ADMIN_EMAIL;
+        $user['canGenerateBundle'] = true;
+    }
+    if ($authorized) {
+        session_start();
+        $_SESSION['userId'] = $user['login'];
+        echo '{ "authorized":true, "user": '.json_encode($user).', "userId": "'.$_SESSION['userId'].'", "sessionId": "'.session_id().'"}';
+        session_write_close();
+    } else {
+        http_response_code(401);
+        echo '{ "authorized":false }';
+    }
+
+?>
---
+++ src/api/zapier/me.php
@@ -0,0 +1,10 @@
+<?php
+
+    include '../config.php';
+    include '../rest_headers.php';
+    include '../init_session.php';
+
+    echo '{ "result": "Welcome!" }';
+
+?> 
+
