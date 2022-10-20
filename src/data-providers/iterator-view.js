2a98d24ca6a8a34d73a27b553dffa5c45aeada52
Renaud Pawlak <renaud.pawlak@gmail.com>
2022-10-20 16:49:31 +0200

    add container layout capabilities to iterators

--- src/data-providers/iterator-view.js
+++ src/data-providers/iterator-view.js
@@ -24,7 +24,10 @@ Vue.component('iterator-view', {
         <div :id="cid">
             <component-icon v-if="edit" :type="viewModel.type"></component-icon>
             <component-badge :component="getThis()" :edit="edit" :targeted="targeted" :selected="selected"></component-badge>
-            <div :class="$eval(viewModel.class)" :style="$eval(viewModel.style)">
+            <div 
+                :style="containerStyle()"
+                :class="containerClass()"             
+            >
                 <component-view v-for="(item, index) in currentPageItems" :key="index" 
                     :cid="viewModel.body ? viewModel.body.cid : undefined" 
                     keyInParent="body" 
@@ -59,6 +62,46 @@ Vue.component('iterator-view', {
         }
     },
     methods: {
+        containerStyle() {
+            let style = 'display: flex; overflow: ' + (this.$eval(this.viewModel.scrollable, false) ? 'auto' : 'visible') + '; flex-direction: ' + (this.$eval(this.viewModel.direction, false) ? this.$eval(this.viewModel.direction) : 'column');
+            if (this.viewModel.wrap) {
+                style += '; flex-wrap: ' + this.$eval(this.viewModel.wrap, '');
+            }
+            if (this.viewModel.justify) {
+                style += '; justify-content: ' + this.toFlexStyle(this.$eval(this.viewModel.justify, ''));
+            }
+            if (this.viewModel.alignItems) {
+                style += '; align-items: ' + this.toFlexStyle(this.$eval(this.viewModel.alignItems, ''));
+            }
+            if (this.viewModel.alignContent) {
+                style += '; align-content: ' + this.toFlexStyle(this.$eval(this.viewModel.alignContent, ''));
+            }
+            if (this.viewModel.rowGap) {
+                style += '; row-gap: ' + this.$eval(this.viewModel.rowGap, '');
+            }
+            if (this.viewModel.columnGap) {
+                style += '; column-gap: ' + this.$eval(this.viewModel.columnGap, '');
+            }
+            if (this.viewModel.backgroundImage) {
+                style += '; background-image: ' + this.$eval(this.viewModel.backgroundImage);
+                style += '; background-size: ' + this.$evalWithDefault(this.viewModel.backgroundSize, 'cover');
+                style += '; background-position: ' + this.$evalWithDefault(this.viewModel.backgroundPosition, 'center');
+            }
+            if (this.viewModel.style) {
+                style += '; ' + this.$eval(this.viewModel.style, '');
+            }
+            return style;
+        },
+        containerClass() {
+            let containerClass = this.componentClass();
+            if (this.viewModel.fillHeight) {
+                containerClass += ' overflow-auto';
+            }
+            if (!this.viewModel.disableContainerPadding) {
+                containerClass += this.viewModel.fixedWidth ? ' container' : ' container-fluid';
+            }
+            return containerClass;
+        },
         currentPageFirstIndex() {
             const perPage = this.$eval(this.viewModel.perPage);
             if (!perPage || perPage === '0' || perPage === '') {
@@ -90,13 +133,74 @@ Vue.component('iterator-view', {
             return [{value:'setCurrentPage',text:'setCurrentPage(pageNumber)'}];
         },
         propNames() {
-            return ["cid", "class", "style", "dataSource", "field", "body", "perPage", "eventHandlers"];
+            return [
+                "cid",
+                "class",
+                "style",
+                "dataSource",
+                "field",
+                "body",
+                "perPage",
+                "fillHeight",
+                "disableContainerPadding",
+                "fixedWidth",
+                "direction",
+                "wrap",
+                "rowGap",
+                "columnGap",
+                "justify",
+                "alignItems",
+                "alignContent",
+                "eventHandlers"
+            ];
         },
         customPropDescriptors() {
             return {
                 body: {
                     type: 'ref',
                     editable: true
+                },
+                fillHeight: {
+                    type: 'checkbox',
+                    description: "Stretch vertically to fill the parent component height",
+                    literalOnly: true
+                },
+                disableContainerPadding: {
+                    type: 'checkbox',
+                    label: 'No padding',
+                    literalOnly: true,
+                    description: "Disable container padding",
+                },
+                fixedWidth: {
+                    type: 'checkbox',
+                    hidden: viewModel => viewModel.disableContainerPadding,
+                    literalOnly: true,
+                    description: "Enable fixed-width padding (full-width is the default - aka fluid)",
+                },
+                direction: {
+                    type: 'select',
+                    options: ['column', 'column-reverse', 'row', 'row-reverse'],
+                    docLink: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/#flex-direction'
+                },
+                wrap: {
+                    type: 'select',
+                    options: ['nowrap', 'wrap', 'wrap-reverse'],
+                    docLink: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/#flex-wrap'
+                },
+                justify: {
+                    type: 'select',
+                    options: ['start', 'end', 'center', 'space-between', 'space-around', 'space-evenly'],
+                    docLink: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/#justify-content'
+                },
+                alignItems: {
+                    type: 'select',
+                    options: ['stretch', 'start', 'end', 'center', 'baseline'],
+                    docLink: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/#align-items'
+                },
+                alignContent: {
+                    type: 'select',
+                    options: ['normal', 'stretch', 'start', 'end', 'center', 'space-between', 'space-around'],
+                    docLink: 'https://css-tricks.com/snippets/css/a-guide-to-flexbox/#align-content'
                 }
             }
         }
